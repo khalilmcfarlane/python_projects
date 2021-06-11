2912eeccd4ed184ce872a3d051c3888d5c467479
khalilmcfarlane <kmac36960@gmail.com>
2021-06-10 22:08:50 -0400

    Update favorite_city.py
    Goal is to make a comprehensive class that asks multiple questions.

--- favorite_city.py
+++ favorite_city.py
@@ -1,9 +1,18 @@
 class FavoriteCity:
     favorite_city_list = []
-    num_players = int(input("How many people do we have here?"))
-    name = input("Welcome. What's your name?")
+    valid_answers = False
+    num_participants = 0
+    while valid_answers:
+        try:
+            num_participants = int(input("How many people do we have here?"))
+            valid_answers = True
+        except ValueError:
+            print("That was not number!")
+    for i in range(num_participants):
+        name = input("Welcome. What's your name?")
+        # call fav city but this will be below the function
 
     def favorite_city(self, name, favorite_city_list=[]):
-        print("Yo, {} .".format(name))
+        print("Yo, {}.".format(name))
         favorite_city_name = input("So, what's your favorite city in the world?")
         favorite_city_list.append(favorite_city_name)
